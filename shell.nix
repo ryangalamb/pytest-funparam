f9b0c2d009fc32db32ccbbfc6e19c105f96c8f32
Ryan J. Miller <rjmiller10@gmail.com>
2021-02-11 17:59:42 -0500

    chore(tox): use tox to manage dev virtualenv

--- shell.nix
+++ shell.nix
@@ -1,22 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    python37
-    python37Packages.virtualenv
+with import <nixpkgs> {};
+let
+  basePythonEnv = python37.withPackages (ps: [
+    ps.setuptools
+    ps.tox
+  ]);
+in mkShell {
+  buildInputs = [
+    basePythonEnv
     python38
     python39
   ];
   shellHook = ''
-    VENV_DIR="$PWD"/env
-    if [[ ! -e $VENV_DIR ]]; then
-      virtualenv -p python3.7 env
-    fi
-
-    . "$VENV_DIR"/bin/activate
-
     # Get rid of PYTHONPATH. We don't need it, and tox makes a bunch of noise
     # about it.
     unset PYTHONPATH
+    # Set up the development virtualenv.
+    tox -e dev
+    # Activate the development virtualenv.
+    . env/bin/activate
   '';
 }
