117604b563c228ed925d0630e498f6dd4d87a8c5
Ryan J. Miller <rjmiller10@gmail.com>
2021-06-05 14:12:32 -0400

    refactor(tests): use plugins instead of import stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,4 @@
-import pytest
-pytest_plugins = 'pytester'
-
-
-pytest.register_assert_rewrite("tests.fixtures.verify_examples")
-
-
-# ORDER MATTERS! This needs to get imported AFTER registering the assert
-# rewriting, otherwise pytest can't rewrite the asserts.
-from .fixtures.verify_examples import (  # noqa
-    verify_examples, verify_one_example
-)
+pytest_plugins = [
+    'pytester',
+    "tests.fixtures.verify_examples",
+]
